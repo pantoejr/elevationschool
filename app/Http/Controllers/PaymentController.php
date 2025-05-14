2159844c8b2b11c6c081a10484cb653bc8200e5f
pantoejr <joelpantoejr@gmail.com>
2025-05-14 07:54:07 +0000

    Refactor PaymentController for code consistency; improve formatting and readability

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -8,48 +8,54 @@ use App\Models\StudentSection;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use App\Models\Payment;
+use Exception;
 use Illuminate\Support\Facades\Auth;
 
 
 class PaymentController extends Controller
 {
-    public function __construct() {
+    public function __construct()
+    {
         $this->middleware(['auth']);
     }
 
-    public function index() {
-        
+    public function index()
+    {
+
         $payments = Payment::all();
         return view('payments.index', [
-            'title'=> 'Payments',
-            'payments'=> $payments
+            'title' => 'Payments',
+            'payments' => $payments
         ]);
     }
 
-    public function create() {
+    public function create()
+    {
         $sections = Section::all();
         return view('payments.create', [
-            'title'=> 'Payments',
-            'sections'=> $sections
+            'title' => 'Payments',
+            'sections' => $sections
         ]);
     }
 
-    public function getStudent(Request $request) {
+    public function getStudent(Request $request)
+    {
 
         $request->validate([
             'section_id' => 'required|exists:sections,id',
         ]);
 
         $students = StudentSection::where('section_id', '=', $request->section_id)
-        ->with('Student')->get();
+            ->with('Student')->get();
 
         return view('payments.studentlist', [
-            'title'=> '',
-            'students'=> $students
+            'title' => '',
+            'students' => $students
         ]);
     }
 
-    public function store(Request $request) {
+    public function store(Request $request)
+    {
 
         $request->validate([
             'section_id' => 'required|exists:sections,id',
@@ -64,16 +70,23 @@ class PaymentController extends Controller
         try {
 
             $paymentHelper = new PaymentHelper();
-            $paymentHelper->makePayment($request->student_id, $request->section_id, 
-            $request->amount, $request->payment_method, Auth::user()->name, $request->currency);
+            $paymentHelper->makePayment(
+                $request->student_id,
+                $request->section_id,
+                $request->amount,
+                $request->payment_method,
+                Auth::user()->name,
+                $request->currency
+            );
 
             return redirect()->route('payments.index')
-            ->with('success','Payment inserted successfully')
-            ->with('flag', 'success');
-        } catch (Exception $e) {
-            return redirect()->route('payments.index')
-            ->with('success','Payment inserted failed')
-            ->with('flag', 'error');
+                ->with('success', 'Payment inserted successfully')
+                ->with('flag', 'success');
+
+        } catch (Exception $ex) {
+            return back()
+                ->with('success', 'Error: ' . $ex->getMessage())
+                ->with('flag', 'error');
         }
     }
 }
